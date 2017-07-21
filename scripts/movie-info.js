aec61ed03bda3844169a9f29157d0adb1be8c4c5
henryph <bloomberg_AMD@cmshk.local>
2017-07-21 17:01:18 +0800

    init the project

---
+++ scripts/movie-info.js
@@ -0,0 +1,97 @@
+var MOVIE_INFO = {
+  '1': {
+    title: 'La La Land',
+    artist: 'Damien Chazelle',
+    release: '2016',
+    poster: 'images/1.jpg',
+    
+    details: [
+      {title: 'Description', content: 'Two proper L.A. dreamers, a suavely charming soft-spoken jazz pianist \
+      and a brilliant vivacious playwright, while waiting for their big break, attempt to reconcile aspirations \
+      and relationship in a magical old-school romance. '},
+      {title: 'Genres', content: 'Comedy, Drama, Music'},
+      {title: 'Length', content: ' 2h 8min'},
+      {title: 'Writers', content: 'Damien Chazelle'},
+      {title: 'Stars', content: 'Ryan Gosling, Emma Stone, Rosemarie DeWitt '},
+    ]
+  },
+  '2': {
+    title: 'Manchester by the sea',
+    artist: 'Kenneth Lonergan',
+    release: '2016',
+    poster: 'images/2.jpg',
+
+
+    details: [
+      {title: 'Description', content: 'A depressed uncle is asked to take care of his teenage nephew after the boy \'s father dies. '},
+      {title: 'Genres', content: 'Drama'},
+      {title: 'Length', content: ' 2h 17min'},
+      {title: 'Writers', content: 'Kenneth Lonergan'},
+      {title: 'Stars', content: 'Casey Affleck, Michelle Williams, Kyle Chandler'},
+    ]
+  },
+  '3': {
+    title: 'Spotlight',
+    artist: 'Tom McCarthy',
+    release: '2015',
+    poster: 'images/3.jpg',
+  
+
+    details: [
+      {title: 'Description', content: 'The true story of how the Boston Globe uncovered the massive scandal of child molestation and cover-up \
+    within the local Catholic Archdiocese, shaking the entire Catholic Church to its core.'},
+      {title: 'Genres', content: 'Crime, Drama, History'},
+      {title: 'Length', content: '2h 8min'},
+      {title: 'Writers', content: 'Josh Singer, Tom McCarthy'},
+      {title: 'Stars', content: 'Mark Ruffalo, Michael Keaton, Rachel McAdams'},
+    ]
+  },
+  '4': {
+    title: 'Dunkirk',
+    artist: ' Christopher Nolan',
+    release: '2017',
+    poster: 'images/4.jpg',
+    details: [
+      {title: 'Description', content: 'Allied soldiers from Belgium, the British Empire and France are surrounded \
+      by the German army and evacuated during a fierce battle in World War II..'},
+      {title: 'Genres', content: 'Action, Drama, History'},
+      {title: 'Length', content: '1h 46min'},
+      {title: 'Writers', content: 'Christopher Nolan'},
+      {title: 'Stars', content: 'Fionn Whitehead, Damien Bonnard, Aneurin Barnard '},
+    ]
+  },
+  '5': {
+    title: 'Inside Out',
+    artist: 'Pete Docter, Ronnie Del Carmen',
+    release: '2015',
+    poster: 'images/5.jpg',
+    
+
+    details: [
+      {title: 'Description', content: 'After young Riley is uprooted from her Midwest life and moved to San Francisco, \
+    her emotions - Joy, Fear, Anger, Disgust and Sadness - conflict on how best to navigate a new city, house, and school.'},
+      {title: 'Genres', content: 'Animation, Adventure, Comedy'},
+      {title: 'Length', content: ' 1h 35min'},
+      {title: 'Writers', content: 'Pete Docter (original story by), Ronnie Del Carmen (original story by)\
+      & 7 more credits'},
+      {title: 'Stars', content: ' Amy Poehler, Bill Hader, Lewis Black'},
+    ]
+  },
+  '6': {
+    title: 'The Secret Life of Pet',
+    artist: 'Chris Renaud, Yarrow Cheney',
+    release: '2015',
+    poster: 'images/6.jpg',
+    
+    details: [
+      {title: 'Description', content: 'The quiet life of a terrier named Max is \
+      upended when his owner takes in Duke, a stray whom Max instantly dislikes.'},
+      {title: 'Genres', content: 'Animation, Adventure, Comedy'},
+      {title: 'Length', content: ' 1h 27min'},
+      {title: 'Writers', content: ' Cinco Paul, Ken Daurio & Brian Lynch'},
+      {title: 'Stars', content: 'Louis C.K., Eric Stonestreet, Kevin Hart '},
+      {title: 'Awards', content: 'Oscar:  Best Animated Feature Film of the Year'}
+
+    ]
+  }
+}
